f2eea372af4b80e6726d2b6fde39cbf2473695c9
KennethVier <kennethcerrado23@gmail.com>
2025-10-20 10:10:14 +0800

    fixed landing page

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -5,7 +5,7 @@ import { Landing } from "./features/landing/landing";
 
 @Component({
   selector: 'app-root',
-  imports: [Button, Landing],
+  imports: [ Landing],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
